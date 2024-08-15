c963b52aaf44c7dd839197e48eb349f1f7fac183
Loogeek <xiaolong3956@gmail.com>
2024-08-15 14:49:18 +0800

    feat: add code preview

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import { PlaygroundContext } from "../../PlaygroundContext";
+import { compile } from "./compiler";
+import iframeRaw from "./iframe.html?raw";
+import { IMPORT_MAP_FILE_NAME } from "../../files";
 
-const Preview: React.FC = () => {
-  return <div className="preview">preview</div>;
-};
+export default function Preview() {
+  const { files } = useContext(PlaygroundContext);
+  const [compiledCode, setCompiledCode] = useState("");
 
-export default Preview;
+  useEffect(() => {
+    const res = compile(files);
+    setCompiledCode(res);
+  }, [files]);
+
+  const getIframeUrl = () => {
+    console.log(files);
+    const res = iframeRaw
+      .replace(
+        '<script type="importmap"></script>',
+        `<script type="importmap">${files[IMPORT_MAP_FILE_NAME].value}</script>`
+      )
+      .replace(
+        '<script type="module" id="appSrc"></script>',
+        `<script type="module" id="appSrc">${compiledCode}</script>`
+      );
+    return URL.createObjectURL(new Blob([res], { type: "text/html" }));
+  };
+
+  useEffect(() => {
+    setIframeUrl(getIframeUrl());
+  }, [files[IMPORT_MAP_FILE_NAME].value, compiledCode]);
+
+  const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
+
+  return (
+    <div style={{ height: "100%" }}>
+      <iframe
+        src={iframeUrl}
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 0,
+          border: "none",
+        }}
+      />
+      {/* <Editor file={{
+            name: 'dist.js',
+            value: compiledCode,
+            language: 'javascript'
+        }}/> */}
+    </div>
+  );
+}
