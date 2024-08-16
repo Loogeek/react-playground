5eb0fbdafb9872fb81d042d7724fcaecc90d57d9
Loogeek <xiaolong3956@gmail.com>
2024-08-16 11:04:27 +0800

    feat: 新增文件列表增删改查功能

--- src/components/CodeEditor/FileNameList/index.tsx
+++ src/components/CodeEditor/FileNameList/index.tsx
@@ -3,30 +3,67 @@ import { useContext, useEffect, useMemo, useState } from "react";
 import FileNameItem from "./FileNameItem";
 
 import styles from "./index.module.scss";
+import {
+  APP_COMPONENT_FILE_NAME,
+  ENTRY_FILE_NAME,
+  IMPORT_MAP_FILE_NAME,
+} from "@/files";
 
 export default function FileNameList() {
   const {
     files,
-    setFiles,
+    addFile,
     selectedFileName,
     setSelectedFileName,
     updateFileName,
+    removeFile,
   } = useContext(PlaygroundContext);
-  // const [tabsName, setTabsName] = useState([])
 
   const tabsName = useMemo(() => Object.keys(files), [files]);
-  console.log("🚀 ~ FileNameList ~ tabsName:", tabsName);
+  const [creating, setCreating] = useState(false);
+
+  const handleEditComplete = (oleName: string, newName: string) => {
+    updateFileName(oleName, newName);
+    setSelectedFileName(newName);
+  };
+
+  const handleAddTab = () => {
+    const newFileName = "Comp" + Math.random().toString().slice(2, 8) + ".tsx";
+    addFile(newFileName);
+    setSelectedFileName(newFileName);
+    setCreating(true);
+  };
+
+  const handleRemoveFile = (name: string) => {
+    removeFile(name);
+    setSelectedFileName(ENTRY_FILE_NAME);
+  };
+
+  const readonlyFileNames = [
+    ENTRY_FILE_NAME,
+    IMPORT_MAP_FILE_NAME,
+    APP_COMPONENT_FILE_NAME,
+  ];
 
   return (
     <div className={styles.tabs}>
-      {tabsName.map((name, index) => (
+      {tabsName.map((name, index, arr) => (
         <FileNameItem
-          name={name}
-          key={index}
+          key={name + index}
+          value={name}
+          readonly={readonlyFileNames.includes(name)}
           actived={selectedFileName === name}
-          onClick={setSelectedFileName}
+          creating={creating && index === arr.length - 1}
+          onClick={() => setSelectedFileName(name)}
+          onEditComplete={(newName) => handleEditComplete(name, newName)}
+          onRemove={() => handleRemoveFile(name)}
         />
       ))}
+      {
+        <div className="cursor-pointer ml-1" onClick={handleAddTab}>
+          +
+        </div>
+      }
     </div>
   );
 }
